c913440ad732774ea9b6e3ba87705d5270845384
4rterxxx <tryaskov.alex@yandex.ru>
2026-01-30 01:59:49 +0300

    readme fix + init py added

--- model/__init__.py
+++ model/__init__.py
@@ -1,13 +1,5 @@
-# model/__init__.py
 """
-Пакет для ML модели линейной регрессии
+Пакет для ML модели предсказания зарплат
 """
-
-__version__ = "1.0.0"
-__all__ = [
-    "LinearRegression", 
-    "SalaryModel", 
-    "ImprovedSalaryModel",
-    "train_and_save_model", 
-    "predict_salaries"
-]
+from .trainer import train_and_save_model
+from .predictor import predict_salaries
